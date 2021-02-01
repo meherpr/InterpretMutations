079da5cd04d16aca7bd871e9ed29e2fdc32aa192
Meher Prakash <42549936+meherpr@users.noreply.github.com>
2021-02-01 09:25:41 +0100

    Add files via upload

---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+python3.7 shapanalysis.py -i ../inputs/blact.csv -o ../outputs/blact/blactfit_0.01 -t 0.25 -f 15 -l 0.01 -n 1247 -d 9 -w 25 -g 0 -s 0.8 -c 0.8 -a 0 > ../outputs/blact/blactfit_0.01.log &
+python3.7 shapanalysis.py -i ../inputs/aph3.csv -o ../outputs/aph3/aph3fit_0.01 -f 14 -t 0.25 -l 0.01 -n 1253 -d 7 -w 15 -g 0 -s 0.8 -c 0.4 -a 0.01 > ../outputs/aph3/aph3fit_0.01.log &
+python3.7 shapanalysis.py -i ../inputs/hsp90.csv -o ../outputs/hsp90/hsp90fit_0.01 -f 14 -t 0.25 -l 0.01 -n 1218 -d 5 -w 1 -g 0 -s 0.6 -c 0.6 -a 0 > ../outputs/hsp90/hsp90fit_0.01.log &
+python3.7 shapanalysis.py -i ../inputs/mapk1.csv -o ../outputs/mapk1/mapk1fit_0.01 -f 14 -t 0.25 -l 0.01 -n 1550 -d 7 -w 25 -g 0 -s 0.7 -c 0.4 -a 0.001 > ../outputs/mapk1/mapk1_0.01.log &
+python3.7 shapanalysis.py -i ../inputs/ube2i.csv -o ../outputs/ube2i/ube2ifit_0.01 -f 14 -t 0.25 -l 0.01 -n 918 -d 7 -w 20 -g 0 -s 0.9 -c 0.4 -a 0.01 > ../outputs/ube2i/ube2ifit_0.01.log
+python3.7 shapanalysis.py -i ../inputs/tpk1.csv -o ../outputs/tpk1/tpk1fit_0.01 -f 14 -t 0.25 -l 0.01 -n 276 -d 7 -w 7 -g 0 -s 0.7 -c 0.4 -a 0.001 > ../outputs/tpk1/tpk1fit_0.01.log &
+python3.7 shapanalysis.py -i ../inputs/bgl3.csv -o ../outputs/bgl3/bgl3fit_0.01 -f 14 -t 0.25 -l 0.01 -n 660 -d 7 -w 5 -g 0 -s 0.9 -c 0.7 -a 0.001 > ../outputs/bgl3/bgl3fit_0.01.log &
+python3.7 shapanalysis.py -i ../inputs/lgk.csv -o ../outputs/lgk/lgkfit_0.01 -f 14 -t 0.25 -l 0.01 -n 804 -d 7 -w 7 -g 0.1 -s 0.7 -c 0.4 -a 0 > ../outputs/lgk/lgkfit_0.01.log
